f68915440a27ac8bf69d6b2ca8820388ef540a7c
Marcus0224 <marcstlouis04@gmail.com>
2022-03-16 18:52:43 -0400

    add features such as add, delete and create for db

--- server.js
+++ server.js
@@ -22,10 +22,37 @@ const db = mysql.createConnection(
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
-  
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
+
+//get a sinlge candidate
+
+// db.query('select * from candidates where id = 1', (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     } console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [8, 'Montague', 'Summers', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
