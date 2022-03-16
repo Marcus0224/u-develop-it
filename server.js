5b2354aac4276323242cb4f3ae727a065015996f
Marcus0224 <marcstlouis04@gmail.com>
2022-03-16 18:36:52 -0400

    err, rows for candidates

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-const express =  require('express');
+const mysql = require('mysql2');
+
+const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MYSQL username,
+        user: 'root',
+        // your MYSQL password
+        password: 'Samantha27!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+  
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
